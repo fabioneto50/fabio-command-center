630c27e6f5372217a884ce31094a32c79a801b33
fabioneto50 <fabioneto@icloud.com>
2026-08-19 17:37:40 +0100

    Cache category navigation module

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE='fcc-master-1.0.2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./content-pack.json','./build-info.json'];
+const CACHE='fcc-master-1.0.3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./navigation-hub.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./content-pack.json','./build-info.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{
-    if(resp && resp.status===200 && resp.type==='basic'){
+    if(resp&&resp.status===200&&resp.type==='basic'){
       const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));
     }
     return resp;
