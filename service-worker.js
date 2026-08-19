f550ff45e2459d4fd0da30e58a314cd8a98df821
fabioneto50 <fabioneto@icloud.com>
2026-08-19 17:30:43 +0100

    Refresh PWA cache for Clinical Tactical redesign

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='fcc-master-1.0.1';
+const CACHE='fcc-master-1.0.2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./content-pack.json','./build-info.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
