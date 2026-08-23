af65735e1b6ac984098b14bbb2d4dee98650fb7f
fabioneto50 <fabioneto@icloud.com>
2026-08-23 03:11:56 +0100

    Bump medication catalogue renderer to V7.4.3

--- medication-info-ux-v5.js
+++ medication-info-ux-v5.js
@@ -1,5 +1,5 @@
 (()=>{
-  const V='923-v7.4.2';
+  const V='923-v7.4.3';
   const load=src=>new Promise((ok,no)=>{const s=document.createElement('script');s.src=`${src}?v=${V}`;s.onload=ok;s.onerror=no;document.head.appendChild(s)});
   load('medication-info-ux-v5-base.js')
     .then(()=>load('medication-catalog-v6.js'))
